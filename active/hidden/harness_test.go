0da5285fd85e5d2defd9a4049901c3712200fe9f
Dustin Decker <dustindecker@protonmail.com>
2020-02-14 08:58:39 -0800

    Vendor
    Signed-off-by: Dustin Decker <dustindecker@protonmail.com>

---
+++ active/hidden/harness_test.go
@@ -0,0 +1,59 @@
+package active
+
+import (
+	"testing"
+
+	"github.com/hashicorp/vault/api"
+)
+
+func TestGetTestCluster(t *testing.T) {
+	type args struct {
+	}
+	tests := []struct {
+		name        string
+		clusterSize int
+	}{
+		{
+			name:        "single node",
+			clusterSize: 1,
+		},
+		{
+			name:        "active-active HA cluster",
+			clusterSize: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cluster := GetTestCluster(t, tt.clusterSize)
+			if cluster == nil {
+				t.Fatal("failed to get test cluster (can it connect to storage?)")
+			}
+			cluster.Start()
+
+			for _, core := range cluster.Cores {
+				client := core.Client
+				_, err := client.Auth().Token().LookupSelf()
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				status, err := client.Sys().SealStatus()
+				if err != nil {
+					t.Fatal(err)
+				}
+				if status.Sealed {
+					t.Fatal("should not be sealed")
+				}
+
+				secret, err := client.Auth().Token().Create(&api.TokenCreateRequest{DisplayName: "lol", TTL: "5"})
+				if err != nil {
+					t.Fatal(err)
+				}
+				if len(secret.Auth.ClientToken) == 0 {
+					t.Fatal("got empty client token")
+				}
+			}
+
+		})
+	}
+}
---
+++ active/hidden/policies_test.go
@@ -0,0 +1,124 @@
+package active
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestPolicies(t *testing.T) {
+	type args struct {
+	}
+	tests := []struct {
+		name        string
+		clusterSize int
+		iterations  int
+	}{
+		{
+			name:        "single node",
+			clusterSize: 1,
+			iterations:  10,
+		},
+		{
+			name:        "active-active HA cluster",
+			clusterSize: 3,
+			iterations:  100,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cluster := GetTestCluster(t, tt.clusterSize)
+			if cluster == nil {
+				t.Fatal("failed to get test cluster (can it connect to storage?)")
+			}
+			cluster.Start()
+
+			var policyTemplate = `
+	path "secret/%s" {
+		capabilities = ["read"]
+	}
+	`
+
+			// Create policies
+			for i := 0; i < tt.iterations; i++ {
+				client := cluster.Cores[rand.Intn(len(cluster.Cores))].Client
+				policyName := fmt.Sprintf("policy-%d", i)
+				err := client.Sys().PutPolicy(policyName, fmt.Sprintf(policyTemplate, "first"))
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			// Read policies
+			for i := 0; i < tt.iterations; i++ {
+				client := cluster.Cores[rand.Intn(len(cluster.Cores))].Client
+				policyName := fmt.Sprintf("policy-%d", i)
+				policy, err := client.Sys().GetPolicy(policyName)
+				if err != nil {
+					t.Fatal(err)
+				}
+				got := strings.TrimSpace(policy)
+				expected := strings.TrimSpace(fmt.Sprintf(policyTemplate, "first"))
+				if got != expected {
+					t.Fatalf("got unexpected first policy: \n%s\n\nexpected:\n\n%s\n", got, expected)
+				}
+			}
+
+			// Update policies
+			for i := 0; i < tt.iterations; i++ {
+				client := cluster.Cores[rand.Intn(len(cluster.Cores))].Client
+				policyName := fmt.Sprintf("policy-%d", i)
+				err := client.Sys().PutPolicy(policyName, fmt.Sprintf(policyTemplate, "second"))
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			// Wait for cache TTL
+			time.Sleep(time.Second)
+
+			// Read policies
+			for i := 0; i < tt.iterations; i++ {
+				client := cluster.Cores[rand.Intn(len(cluster.Cores))].Client
+				policyName := fmt.Sprintf("policy-%d", i)
+				policy, err := client.Sys().GetPolicy(policyName)
+				if err != nil {
+					t.Fatal(err)
+				}
+				got := strings.TrimSpace(policy)
+				expected := strings.TrimSpace(fmt.Sprintf(policyTemplate, "second"))
+				if got != expected {
+					t.Fatalf("got unexpected first policy: \n%s\n\nexpected:\n\n%s\n", got, expected)
+				}
+			}
+
+			// Delete policies
+			for i := 0; i < tt.iterations; i++ {
+				client := cluster.Cores[rand.Intn(len(cluster.Cores))].Client
+				policyName := fmt.Sprintf("policy-%d", i)
+				err := client.Sys().DeletePolicy(policyName)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			// Wait for cache TTL
+			time.Sleep(time.Second)
+
+			// Read policies
+			for i := 0; i < tt.iterations; i++ {
+				client := cluster.Cores[rand.Intn(len(cluster.Cores))].Client
+				policyName := fmt.Sprintf("policy-%d", i)
+				policy, err := client.Sys().GetPolicy(policyName)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if len(policy) > 0 {
+					t.Fatal("expected empty policy")
+				}
+			}
+		})
+	}
+}
